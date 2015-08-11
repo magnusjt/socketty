22851cd9cd805dd0dc990c49351974d57b7bc4f7
Magnus Tovslid <magnus.tovslid@gmail.com>
2015-08-11 09:47:22 +0200

    Expose more in npm module. Need actions to be able to connect

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./client/js/Socketty.jsx');
+var React = require('react');
+import {connect} from './client/js/Actions.js';
+import Socketty from './client/js/Socketty.jsx';
+
+module.exports.start = function(domId, wssUrl, opts){
+    connect(wssUrl);
+    React.render(React.createElement(Socketty, {opts}), document.getElementById(domId));
+};
